51c3c057e3880487fae1c45a42da0c57231d0657
Johnny Kamprath <johnnykamprath@gmail.com>
2021-02-19 14:00:05 -0600

    Create console app to run Valheim server and forward console output

---
+++ ValheimBackup/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.IO;
+using System.Diagnostics;
+
+namespace ValheimBackup
+{
+    class Program
+    {
+        private const string ServerName = "Test Server";
+        private const int Port = 2456;
+        private const string Password = "secret";
+        
+        static void Main(string[] args)
+        {
+            if (args == null || args.Length == 0)
+            {
+                Console.WriteLine("Usage: ValheimManager.exe <path to Valheim executable>");
+                return;
+            }
+
+            var filePath = args[0];
+
+            if (!IsStartupFileValid(filePath))
+            {
+                Console.WriteLine("Invalid startup file. Please specify the path to your Valheim server startup file.");
+                return;
+            }
+            
+            using (Process process = new Process())
+            {
+                process.StartInfo.UseShellExecute = false;
+                process.StartInfo.RedirectStandardOutput = true;
+                process.StartInfo.FileName = filePath;
+                process.StartInfo.Arguments = $"-nographics -batchmode -name \"{ServerName}\" -port {Port} -world \"Dedicated\" -password \"{Password}\"";
+                process.StartInfo.Environment.Add("SteamAppId", "892970");
+
+                process.OutputDataReceived += (sender, e) => Console.WriteLine(e.Data);
+                process.ErrorDataReceived += (sender, e) => Console.WriteLine($"ERROR: {e.Data}");
+
+                process.Start();
+                process.BeginOutputReadLine();
+                process.WaitForExit();
+            }
+            
+            Console.WriteLine("\nPress any key to exit...");
+            Console.Read();
+        }
+
+        static bool IsStartupFileValid(string filePath)
+        {
+            if (!File.Exists(filePath))
+            {
+                Console.WriteLine("File does not exist.");
+                return false;
+            }
+
+            var fileInfo = new FileInfo(filePath);
+
+            if (fileInfo.Extension != ".exe")
+            {
+                Console.WriteLine("File is not an executable.");
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
