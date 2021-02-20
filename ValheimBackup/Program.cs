1f84341170a2729d6c22fdfc7b8890212e9d6f59
Johnny Kamprath <johnnykamprath@gmail.com>
2021-02-20 12:14:54 -0600

    Refactor code + add "event handling"

--- ValheimBackup/Program.cs
+++ ValheimBackup/Program.cs
@@ -6,6 +6,7 @@ namespace ValheimBackup
 {
     class Program
     {
+        // todo: store this info in a config file
         private const string ServerName = "Test Server";
         private const string World = "Dedicated";
         private const string Password = "secret";
@@ -16,7 +17,7 @@ namespace ValheimBackup
         {
             if (args == null || args.Length == 0)
             {
-                Console.WriteLine("Usage: ValheimManager.exe <path to Valheim executable>");
+                Console.WriteLine("Usage: ValheimManager.exe <path to valheim_server.exe>");
                 return;
             }
 
@@ -24,28 +25,13 @@ namespace ValheimBackup
 
             if (!IsStartupFileValid(filePath))
             {
-                Console.WriteLine("Invalid startup file. Please specify the path to your Valheim server startup file.");
+                Console.WriteLine("Invalid startup file. Please specify filepath to valheim_server.exe");
                 return;
             }
-            
-            using (Process process = new Process())
-            {
-                process.StartInfo.UseShellExecute = false;
-                process.StartInfo.RedirectStandardOutput = true;
-                process.StartInfo.FileName = filePath;
-                process.StartInfo.Arguments = $"-nographics -batchmode -name \"{ServerName}\" -port {Port} -world \"{World}\" -password \"{Password}\"";
-                process.StartInfo.Environment.Add("SteamAppId", SteamAppId);
 
-                process.OutputDataReceived += (sender, e) => Console.WriteLine(e.Data);
-                process.ErrorDataReceived += (sender, e) => Console.WriteLine($"ERROR: {e.Data}");
+            ConsoleWrite("Starting Valheim server...");
 
-                process.Start();
-                process.BeginOutputReadLine();
-                process.WaitForExit();
-            }
-            
-            Console.WriteLine("\nPress any key to exit...");
-            Console.Read();
+            StartServerExecutable(filePath);
         }
 
         static bool IsStartupFileValid(string filePath)
@@ -65,6 +51,106 @@ namespace ValheimBackup
             }
 
             return true;
+        }
+
+        static void StartServerExecutable(string filePath)
+        {
+            using (Process process = new Process())
+            {
+                ConfigureStartInfo(process.StartInfo, filePath);
+
+                process.OutputDataReceived += (sender, e) => HandleOutputDataReceived(e.Data);
+                process.ErrorDataReceived += (sender, e) => ConsoleWrite($"[ERROR] {e.Data}");
+                
+                // kill process when Ctrl + C is used to stop the console app
+                Console.CancelKeyPress += (sender, e) =>
+                {
+                    ConsoleWrite("Stopping server...");
+                    process.Close();
+                    process.Dispose();
+                };
+
+                process.Start();
+                process.BeginOutputReadLine();
+                process.WaitForExit();
+            }
+        }
+
+        static void ConfigureStartInfo(ProcessStartInfo startInfo, string filePath)
+        {
+            startInfo.Arguments = $"-nographics -batchmode -name \"{ServerName}\" -port {Port} -world \"{World}\" -password \"{Password}\"";
+            startInfo.FileName = filePath;
+            
+            startInfo.UseShellExecute = false;
+            startInfo.RedirectStandardOutput = true;
+            
+            startInfo.Environment.Add("SteamAppId", SteamAppId);
+        }
+
+        static bool HandleOutputDataReceived(string output)
+        {
+            // todo: write output to log file
+            
+            if (output.Contains("(Filename: ")) return false;
+            if (output.Trim() == "") return false;
+            
+            if (output.Contains("Game server connected")) return OnWorldReady(output);
+            if (output.Contains(": Got character ZDOID from ")) return OnPlayerConnect(output);
+            if (output.Contains(": Closing socket ")) return OnPlayerDisconnect(output);
+            if (output.Contains(":  Connections ")) return OnConnectionCount(output);
+            if (output.Contains("World saved (")) return OnWorldSave();
+            
+            Console.WriteLine($"Debug: {output}");
+            return true;
+        }
+
+        static bool OnWorldReady(string output)
+        {
+            ConsoleWrite("Server ready!");
+            return true;
+        }
+
+        static bool OnPlayerConnect(string output)
+        {
+            var startStr = "Got character ZDOID from ";
+            var startPos = output.IndexOf(startStr, StringComparison.Ordinal) + startStr.Length;
+
+            var tempString = output.Substring(startPos);
+            var playerName = tempString.Substring(0, tempString.IndexOf(" : ", StringComparison.Ordinal));
+
+            ConsoleWrite($"Player {playerName} connected");
+            return true;
+        }
+
+        static bool OnPlayerDisconnect(string output)
+        {
+            var startStr = ": Closing socket ";
+            var playerId = output.Substring(
+                output.IndexOf(startStr, StringComparison.Ordinal) + startStr.Length
+            ).Trim();
+
+            if (playerId == "0") return false;
+            
+            ConsoleWrite($"Player disconnected ({playerId})");
+            return true;
+        }
+
+        static bool OnConnectionCount(string output)
+        {
+            return true;
+        }
+
+        static bool OnWorldSave()
+        {
+            // todo: create world backup
+            ConsoleWrite("World saved.");
+            return true;
+        }
+
+        static void ConsoleWrite(string message)
+        {
+            var now = DateTime.Now;
+            Console.WriteLine($"[{now.Month}/{now.Day}/{now.Year} {now.Hour}:{now.Minute}:{now.Second}]  {message}");
         }
     }
 }
